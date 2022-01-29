28952ad4beb61afdb9c57ec54cfdec8962c06db8
Mateus Avila <mateus.avila@abacomm.com.br>
2022-01-28 22:57:57 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+  import { Swiper, SwiperSlide } from 'swiper/vue'
+  import 'swiper/scss'
+  const title = ref<string>('mateus')
+</script>
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <swiper
+      :slides-per-view="1"
+      :space-between="50"
+    >
+      <swiper-slide>Slide 1</swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide>
+      <swiper-slide>Slide 3</swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<style lang="sass">
+  h1
+    font-family: 'Tahoma', sans-serif
+    
+</style>
